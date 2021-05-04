f457a496dc845026fd42af84aa1d00b9d4aad7b4
msalim <msalim@anl.gov>
2021-05-03 22:12:59 -0500

    updated

--- 00_workflows/balsam_demo/1-hello/example.sh
+++ 00_workflows/balsam_demo/1-hello/example.sh
@@ -6,7 +6,7 @@ balsam app --name hello --executable "echo hello, "
 
 # Add a couple instances of that app:
 balsam job --name hello-world --workflow demo-hello   --app hello --args 'world!'  --yes
-balsam job --name hello-SDL   --workflow demo-hello   --app hello --args 'SDL workshop!' --ranks-per-node 2 --yes
+balsam job --name hello-workshop   --workflow demo-hello   --app hello --args 'workshop!' --ranks-per-node 2 --yes
 
 # View your tasks:
 balsam ls
@@ -15,7 +15,7 @@ BALSAM_LS_FIELDS=ranks_per_node:args balsam ls
 
 # Now submit a job to run those tasks
 # Important: Please modify the project (-A) and (-q) as necessary for your allocation/machine:
-balsam submit-launch -n 1 -q training -A SDL_Workshop -t 5 --job-mode mpi
+balsam submit-launch -n 1 -q training -A Comp_Perf_Workshop -t 5 --job-mode mpi
 
 # Use `qstat` to track your Cobalt job
 # Use `balsam ls` to track the status of each task within a job
