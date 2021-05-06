90a64a0f8e6bdb9cfb9ecec93d98fbab8168dae7
Murali Emani <15612272+memani1@users.noreply.github.com>
2021-05-06 09:48:01 -0500

    Update run_pt_mnist.sh

--- 14_mps-mig-A100/run_pt_mnist.sh
+++ 14_mps-mig-A100/run_pt_mnist.sh
@@ -4,7 +4,7 @@
 module load conda/pytorch/2021-03-02 
 conda activate
 
-# set proxies
+# set proxies or use --attrs pubnet=true while requesting a node
 export http_proxy=http://theta-proxy.tmi.alcf.anl.gov:3128
 export https_proxy=https://theta-proxy.tmi.alcf.anl.gov:3128
 
