7d7a764ae616ad3f7c0830d3d1ef9a1d063e5c79
Sam Foreman <saforem2@gmail.com>
2022-05-13 10:14:25 -0500

    Updates `horovod/torch/main.py`

--- 05_scaling-DL/src/cpw/horovod/torch/main.py
+++ 05_scaling-DL/src/cpw/horovod/torch/main.py
@@ -1,17 +1,16 @@
 """
 horovod/torch/main.py
 
-Contains simple implementation of using Hoorovod for data parallel distributed training.
+Contains simple implementation of using Hoorovod for data parallel distributed
+training.
 """
 from __future__ import absolute_import, annotations, division, print_function
-import json
 import logging
-import os
 import time
-from typing import Callable
+from typing import Union
 
 import hydra
-from omegaconf import DictConfig
+import numpy as np
 from omegaconf import DictConfig
 import torch
 from torch.cuda.amp.grad_scaler import GradScaler
@@ -37,6 +36,18 @@ log = logging.getLogger(__name__)
 
 Tensor = torch.Tensor
 WITH_CUDA = torch.cuda.is_available()
+
+
+def metrics_to_str(m: dict[str, Union[Tensor, float]]) -> str:
+    strs = {}
+    for key, val in m.items():
+        if isinstance(val, Tensor):
+            if len(val.shape) > 1:
+                val = val.mean()
+
+        strs[key] = f'{val:.5f}'
+
+    return ', '.join([f'{k}: {v}' for (k, v) in strs.items()])
 
 
 class Net(nn.Module):
@@ -73,8 +84,6 @@ class Trainer:
 
         self.data = self.setup_data()
         self.model = self.build_model()
-        if self.device == 'gpu':
-            self.model.cuda()
         if self.device == 'gpu':
             self.model.cuda()
 
@@ -181,10 +190,8 @@ class Trainer:
         if self.device == 'gpu':
             data, target = data.cuda(), target.cuda()
         probs = self.model(data)
-        self.optimizer.zero_grad()
         loss = self.loss_fn(probs, target)
-        loss.backward()
-        self.optimizer.step()
+        self.optimizer.zero_grad()
         if self.scaler is not None:
             self.scaler.scale(loss).backward()
             self.scaler.step(self.optimizer)
@@ -210,14 +217,13 @@ class Trainer:
         if WITH_CUDA:
             data, target = data.cuda(), target.cuda()
 
-        with torch.no_grad():
-            probs = self.model(data)
-            loss = self.loss_fn(probs, target)
-            pred = probs.data.max(1, keepdim=True)[1]
-            acc = pred.eq(target.data.view_as(pred)).cpu().float().sum()
+        probs = self.model(data)
+        loss = self.loss_fn(probs, target)
+        pred = probs.data.max(1, keepdim=True)[1]
+        acc = pred.eq(target.data.view_as(pred)).cpu().float().sum()
 
-            # _, pred = probs.data.max(1)
-            # acc = (pred == target).sum() / pred.shape[0]
+        # _, pred = probs.data.max(1)
+        # acc = (pred == target).sum() / pred.shape[0]
 
         return loss, acc
 
@@ -237,29 +243,36 @@ class Trainer:
         train_loader = self.data['train']['loader']
         # Horovod: set epoch to sampler for shuffling
         train_sampler.set_epoch(epoch)
+        nsamples = len(train_sampler)
         # ntrain = len(train_loader.dataset) // SIZE // self.cfg.batch_size
         # size = len(train_loader.dataset)
         metrics_ = {}
         for bidx, (data, target) in enumerate(train_loader):
             loss, acc = self.train_step(data, target)
-            training_acc += acc
-            running_loss += loss
+            training_acc = training_acc + acc
+            running_loss = running_loss + loss.item()
+            # training_acc += acc
+            # running_loss += loss.item()
             metrics_ = {
                 'epoch': epoch,
                 'elapsed:': time.time() - start,
                 'batch_loss': loss.item() / self.cfg.batch_size,
-                'running_loss': running_loss / len(data.sampler),
+                'running_loss': running_loss / nsamples,
                 'batch_acc': acc.item() / self.cfg.batch_size,
-                'training_acc': training_acc / len(data.sampler),
+                'training_acc': training_acc / nsamples,
             }
 
             if bidx % self.cfg.logfreq == 0 and RANK == 0:
                 jdx = bidx * len(data)
-                frac = 100. * bidx / len(data.loader)
+                frac = 100. * bidx / len(self.data['train']['sampler'])
                 pre = [f'[{RANK}]',
-                       f'[{jdx:>5}/{len(data.sampler):<5} ({frac:>03.1f}%)]']
-                mstr = [f'{k}: {v:.5f}' for (k, v) in metrics_.items()]
-                log.info(f'{pre} {mstr}')
+                       f'[{jdx:>5}/{nsamples:<5} ({frac:>03.1f}%)]']
+                mstr = ' '.join([
+                    f'{k}: {v:.4f}' for (k, v) in metrics_.items()
+                ])
+                log.info(' '.join([*pre, mstr]))
+                # mstr = [f'{v[0]}: {v[1]:.5f}' for v in metrics_.items()]
+                # log.info(f'{pre} {metrics_to_str(metrics_)}')
 
         return metrics_
 
@@ -337,8 +350,10 @@ class Trainer:
         # )
         for data, target in self.data['test']['loader']:
             loss, acc = self.test_step(data, target)
-            running_acc += acc
-            running_loss += loss
+            running_acc = running_acc + acc
+            running_loss = running_loss + loss.item()
+            # running_acc += acc
+            # running_loss += loss.item()
             # test_loss += F.nll_loss(output, target).item()
             # get the index of the max log-probability
             # running_acc += pred.eq(target.data.view_as(pred)).float().sum()
@@ -347,8 +362,8 @@ class Trainer:
 
         # DDP: use test_sampler to determine
         # the number of examples in this workers partition
-        running_loss /= len(self.data['test']['sampler'])
-        running_acc /= len(self.data['test']['sampler'])
+        running_loss = running_loss / len(self.data['test']['sampler'])
+        running_acc = running_acc / len(self.data['test']['sampler'])
 
         # DDP: average metric values across workers
         running_loss = metric_average(running_loss, 'loss')
@@ -386,193 +401,24 @@ def evaluate(model, test_loader):
     return accuracy
 
 
-LossFunction = Callable[[torch.Tensor, torch.Tensor], torch.Tensor]
-
-def train(
-        epoch: int,
-        data: DistributedDataObject,
-        rank: int,
-        model: nn.Module,
-        loss_fn: LossFunction,
-        optimizer: optim.Optimizer,
-        args: dict,
-        scaler: GradScaler=None,
-):
-    model.train()
-    # Horovod: set epoch to sampler for shuffling
-    data.sampler.set_epoch(epoch)
-    running_loss = torch.tensor(0.0)
-    training_acc = torch.tensor(0.0)
-
-    with_cuda = torch.cuda.is_available()
-
-    if with_cuda:
-        running_loss = running_loss.cuda()
-        training_acc = training_acc.cuda()
-
-    for batch_idx, (batch, target) in enumerate(data.loader):
-        #  if torch.cuda.is_available():
-        if with_cuda:
-            batch, target = batch.cuda(), target.cuda()
-
-        optimizer.zero_grad()
-        output = model(batch)
-        loss = loss_fn(output, target)
-
-        if scaler is not None:
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
-        else:
-            loss.backward()
-            optimizer.step()
-
-        pred = output.data.max(1, keepdim=True)[1]
-        acc = pred.eq(target.data.view_as(pred)).cpu().float().sum()
-
-        training_acc += acc
-        running_loss += loss.item()
-
-        if batch_idx % args.log_interval == 0:
-            metrics_ = {
-                'epoch': epoch,
-                'batch_loss': loss.item() / args.batch_size,
-                'running_loss': running_loss / len(data.sampler),
-                'batch_acc': acc.item() / args.batch_size,
-                'training_acc': training_acc / len(data.sampler),
-            }
-
-            jdx = batch_idx * len(batch)
-            frac = 100. * batch_idx / len(data.loader)
-            pre = [f'[{rank}]',
-                   f'[{jdx:>5}/{len(data.sampler):<5} ({frac:>03.1f}%)]']
-            mstr = [f'{k}: {v:.5f}' for (k, v) in metrics_.items()]
-            log.info(f'{pre} {mstr}')
-
-    running_loss = running_loss / len(data.sampler)
-    training_acc = training_acc / len(data.sampler)
-    loss_avg = metric_average(running_loss, 'running_loss')
-    training_acc = metric_average(training_acc, 'training_acc')
-    if rank == 0:
-        log.info(f'[TRAIN] loss: {loss_avg:.4f}')
-        log.info(f'[TRAIN] acc: {training_acc * 100:.2f}%')
-
-        # logger.log(f'training set; avg loss: {loss_avg:.4g}, '
-        #           f'accuracy: {training_acc * 100:.2f}%')
-
-
-def main():
-    # Horovod: initialize library.
-    hvd.init()
-    torch.manual_seed(args.seed)
-    local_rank = hvd.local_rank()
-    SIZE = hvd.size()
-
-    if args.cuda:
-        device = torch.device(f'cuda:{local_rank}')
-        # Horovod: pin GPU to local rank.
-        torch.cuda.set_device(device)
-        torch.cuda.manual_seed(args.seed)
-
-    # Horovod: limit # of CPU threads to be used per worker.
-    torch.set_num_threads(1)
-
-    kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-    # When supported, use 'forkserver' to spawn dataloader workers instead of 'fork' to prevent
-    # issues with Infiniband implementations that are not fork-safe
-    if (kwargs.get('num_workers', 0) > 0 and hasattr(mp, '_supports_context') and
-            mp._supports_context and 'forkserver' in mp.get_all_start_methods()):
-        kwargs['multiprocessing_context'] = 'forkserver'
-
-    # Horovod: use DistributedSampler to partition the training data.
-    data = prepare_datasets(args, rank=local_rank, num_workers=SIZE, data='mnist')
-    model = Net()
-
-    # By default, Adasum doesn't need scaling up learning rate.
-    lr_scaler = hvd.size() if not args.use_adasum else 1
-
-    if args.cuda:
-        # Move model to GPU.
-        model.cuda()
-        # If using GPU Adasum allreduce, scale learning rate by local_size.
-        if args.use_adasum and hvd.nccl_built():
-            lr_scaler = hvd.local_size()
-
-    # Horovod: scale learning rate by lr_scaler.
-    optimizer = optim.SGD(model.parameters(),
-                          lr=args.lr*lr_scaler,
-                          momentum=args.momentum)
-
-    # Horovod: (optional) compression algorithm.
-    compression = (
-        hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.none
-    )
-
-    # Horovod: wrap optimizer with DistributedOptimizer.
-    optimizer = hvd.DistributedOptimizer(
-        optimizer,
-        named_parameters=model.named_parameters(),
-        compression=compression,
-        op=hvd.Adasum if args.use_adasum else hvd.Average,
-        gradient_predivide_factor=args.gradient_predivide_factor
-    )
-
-    # Horovod: broadcast parameters & optimizer state.
-    hvd.broadcast_parameters(model.state_dict(), root_rank=0)
-    hvd.broadcast_optimizer_state(optimizer, root_rank=0)
-
-    loss_fn = nn.CrossEntropyLoss()
-    epoch_times = []
-
-    for epoch in range(1, args.epochs + 1):
-        t0 = time.time()
-        train(epoch, data['training'], rank=local_rank,
-              model=model, loss_fn=loss_fn,
-              optimizer=optimizer, args=args, scaler=None)
-
-        if epoch > 2:
-            epoch_times.append(time.time() - t0)
-
-        if epoch % 10 == 0:
-            if hvd.local_rank() == 0:
-                accuracy = evaluate(model=model,
-                                    test_loader=data['testing'].loader)
-                logger.log('-' * 75)
-                logger.log(f'Epoch: {epoch}, Accuracy: {accuracy}')
-                logger.log('-' * 75)
-
-
-    if local_rank == 0:
-        epoch_times_str = ', '.join(str(x) for x in epoch_times)
-        logger.log('Epoch times:')
-        logger.log(epoch_times_str)
-
-        outdir = os.path.join(os.getcwd(), 'results_mnist', f'size{SIZE}')
-        if not os.path.isdir(outdir):
-            os.makedirs(outdir)
-
-        modeldir = os.path.join(outdir, 'saved_models')
-        modelfile = os.path.join(modeldir, 'hvd_model_mnist.pth')
-        if not os.path.isdir(modeldir):
-            os.makedirs(modeldir)
-
-        logger.log(f'Saving model to: {modelfile}')
-        torch.save(model.state_dict(), modelfile)
-
-        args_file = os.path.join(outdir, f'args_size{SIZE}.json')
-        logger.log(f'Saving args to: {args_file}.')
-
-        with open(args_file, 'at') as f:
-            json.dump(args.__dict__, f, indent=4)
-
 @hydra.main(config_path='./conf', config_name='config')
-def main(cfg: DictConfig):
-    epoch_times_str = ', '.join(str(x) for x in epoch_times)
-    times_file = os.path.join(outdir,
-                              f'epoch_times_size{SIZE}.csv')
-    log.info(f'Saving epoch times to: {times_file}')
-    with open(times_file, 'a') as f:
-        f.write(epoch_times_str + '\n')
+def main(cfg: DictConfig) -> None:
+    start = time.time()
+
+    epoch_times = []
+    trainer = Trainer(cfg)
+    for epoch in range(cfg.epochs):
+        t0 = time.time()
+        _ = trainer.train_epoch(epoch)
+        _ = trainer.test()
+        epoch_times.append(time.time() - t0)
+        # if RANK == 0:
+        #    trainer.save_checkpoint()
+
+    log.info(f'Total training time: {time.time() - start} seconds')
+    log.info(
+        f'Average time per epoch in the last 5: {np.mean(epoch_times[-5])}'
+    )
 
 
 if __name__ == '__main__':
