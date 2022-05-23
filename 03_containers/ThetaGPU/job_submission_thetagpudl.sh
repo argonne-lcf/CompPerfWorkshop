34c1886daea049cd636d6d9ccd307ba14c9dfb19
Taylor Childers <taylor@jtchilders.com>
2022-05-23 19:40:26 +0000

    update job script

--- 03_containers/ThetaGPU/job_submission_thetagpudl.sh
+++ 03_containers/ThetaGPU/job_submission_thetagpudl.sh
@@ -8,15 +8,14 @@ CONTAINER=$1
 export http_proxy=http://proxy.tmi.alcf.anl.gov:3128
 export https_proxy=http://proxy.tmi.alcf.anl.gov:3128
 
+wget https://github.com/horovod/horovod/raw/master/examples/pytorch/pytorch_synthetic_benchmark.py
+
 NODES=`cat $COBALT_NODEFILE | wc -l`
 PPN=8 # GPUs per NODE
 PROCS=$((NODES * PPN))
 echo NODES=$NODES  PPN=$PPN  PROCS=$PROCS
 
-MPI_BASE=/lus/theta-fs0/software/thetagpu/openmpi-4.0.5/
-export LD_LIBRARY_PATH=$MPI_BASE/lib:$LD_LIBRARY_PATH
-export SINGULARITYENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-echo mpirun=$(which mpirun)
-
 echo test mpi
-mpirun -hostfile $COBALT_NODEFILE -n $PROCS -npernode $PPN singularity exec --nv -B $MPI_BASE $CONTAINER python -c "import horovod.torch as hvd;hvd.init();print(hvd.local_rank(),hvd.rank(),hvd.size())"
+mpirun -hostfile $COBALT_NODEFILE -n $PROCS -npernode $PPN \
+   singularity exec --nv -B $PWD $CONTAINER \
+      python $PWD/pytorch_synthetic_benchmark.py
