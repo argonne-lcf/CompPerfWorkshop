5e255dd9cf08efbaca29b52e5eca4991b8e20146
Thomas Uram <turam@mcs.anl.gov>
2022-05-24 15:27:59 +0000

    Update

--- 00_workflows/balsam_demo/0_setup.sh
+++ 00_workflows/balsam_demo/0_setup.sh
@@ -1,10 +1,10 @@
 # Create a virtual environment
 /lus/theta-fs0/software/datascience/conda/2021-09-22/mconda3/bin/python -m venv env
 source env/bin/activate
-python -m pip install --upgrade pip
+pip install --upgrade pip
 
 # Install Balsam
-python -m pip install balsam
+pip install balsam
 
 # Login to the Balsam server. This will prompt you to visit an ALCF login page; this command will
 # block until you've logged in on the webpage.
